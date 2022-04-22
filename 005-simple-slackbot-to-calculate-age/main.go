6f94bcbb88a5b8128d7da0804c58fccb60b97f55
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-04-22 17:18:26 +0300

    add

--- 005-simple-slackbot-to-calculate-age/main.go
+++ 005-simple-slackbot-to-calculate-age/main.go
@@ -5,9 +5,10 @@ import(
 	"context"
 	"os"
 	"log"
+	"strconv"
 
 	 "github.com/joho/godotenv"
-	_"github.com/shomali11/slacker"
+	"github.com/shomali11/slacker"
 )
 
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
@@ -28,14 +29,15 @@ func main() {
     log.Fatal("Error loading .env file")
   }
 
-	os.Setenv("SLACK_BOT_TOKEN","")
-	os.Setenv("SLACK_APP_TOKEN","")
+  //create/use your own tokens, these tokens not exist.
+	os.Setenv("SLACK_BOT_TOKEN","xoxb-3401875288404-3399548909602-xYlxrd95sLIa2DFkQ0yK2enI")
+	os.Setenv("SLACK_APP_TOKEN","xapp-1-A03C44F48U9-3399535889266-94f97b18b9eb03c153a1b58c7c2b380a364c2ff7afd9dac48578bcecf06bbfb5")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_APP_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("my yob is <year>", &slacker.CommandDefinition){
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
 		Example: "my yob is 2020",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter){
@@ -47,13 +49,13 @@ func main() {
 			age := 2022 - yob
 			r := fmt.Sprintf("age is %d ", age)
 			response.Reply(r)
-		}
-	}
+		},
+	})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err := bot.Listen(ctx)
+	err = bot.Listen(ctx)
 	if err != nil{
 		log.Fatal(err)
 	}
