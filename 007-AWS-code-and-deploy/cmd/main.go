e69df5469ea63c50941c9699c89698e5be290bc9
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-04-26 17:37:17 +0300

    add

--- 007-AWS-code-and-deploy/cmd/main.go
+++ 007-AWS-code-and-deploy/cmd/main.go
@@ -3,12 +3,37 @@ package main
 import(
 	"fmt"
 	"os"
+
+	"github.com/aws/aws-lambada-go/events"
+	_"github.com/aws/aws-lambada-go/lambada"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+const tableName = "lambdaInGoUser"
+
+func handler(req events.APIGetewayProxyRequest)(*events.APIGetewayProxyRequest, error){
+	switch req.HTTPMethod {
+		case "GET":
+			return handlers.GetUser(req, tableName, dynaClient)
+		case "POST":
+			return handlers.CreateUser(req,tableName, dynaClient)
+		case "PUT":
+			return handlers.UpdateUser(req, tableName, dynaClient)
+		case "DELETE":
+			return handlers.UnhandledMethod()
+	}
+}
 
 func main() {
 	region := os.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
 		Region: aws.String(region)
 	},)
+	if err != nil{
+		return nil
+	}
+	dynaClient = dynamodb.New(awsSession)
+	lambda.Start(handler)
 }
--- 007-AWS-code-and-deploy/go.mod
+++ 007-AWS-code-and-deploy/go.mod
@@ -1,3 +1,8 @@
 module github.com/FuaAlfu/Go-Projects/007-AWS-code-and-deploy
 
 go 1.18
+
+require (
+	github.com/aws/aws-sdk-go v1.44.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
