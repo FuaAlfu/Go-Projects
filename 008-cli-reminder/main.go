33660d310a1d30bed11c3bba71b6d8fbaf34bdc9
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-05-10 13:42:32 +0300

    add

--- 008-cli-reminder/main.go
+++ 008-cli-reminder/main.go
@@ -20,5 +20,26 @@ const(
 func main(){
 	if len(os.Args) > 3{
 		fmt.Println("Usage: %s <hh:mm> <text message\n>",os.Args[0])
+		os.Exit(1)
+	}
+
+	now := time.Now()
+
+	w := when.New(nil)
+	w.Add(en.All...)
+	w.Add(common.All...)
+
+	t, err := w.Parse(os.Args[1],now)
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	if t == nil{
+		fmt.Println("Unable to parse time..")
+		os.Exit(2)
+	}
+	if now.After(t.Time){
+		fmt.Println("set a future time")
+		os.Exit(3)
 	}
 }
