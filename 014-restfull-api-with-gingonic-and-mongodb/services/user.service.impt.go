960ae2b09f42baedc868043de2246fac56eb13fe
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-07-06 13:33:37 +0300

    add

--- 014-restfull-api-with-gingonic-and-mongodb/services/user.service.impt.go
+++ 014-restfull-api-with-gingonic-and-mongodb/services/user.service.impt.go
@@ -1,15 +1,40 @@
 package services
 
-type UserServiceimpl struct{
+import( 
+	"context"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/FuaAlfu/Go-Projects/014-restfull-api-with-gingonic-and-mongodb/models"
+)
+
+type UserServiceimpl struct{
+	usercollection *mongo.Collection
+	ctx             context.Context
 }
 
-func GetAll(){}
+func NewUserService(usercollection *mongo.Collection, ctx context.Context) UserService{
+	return &UserServiceimpl{
+		usercollection: usercollection,
+		ctx:            ctx,
+	}
+}
 
-func CreateUser(){}
+func (u *UserServiceimpl) GetAll([] *models.User, error){
+	return nil, nil
+}
 
-func GetUser(){}
+func (u *UserServiceimpl) CreateUser(user *models.User) error{
+	return nil
+}
 
-func UpdateUser(){}
+func (u *UserServiceimpl) GetUser(name string)(*models.User, error){
+	return nil, nil
+}
 
-func DeleteUser()
+func (u *UserServiceimpl) UpdateUser(user *models.User) error{
+	return nil
+}
+
+func (u *UserServiceimpl) DeleteUser(name string) error{
+	return nil
+}
