e53069ebd87afb23b3b2cdf5fcdc0f2f0b79bd06
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-01-02 23:02:14 +0300

    add

--- 025-go-api-echo/cmd/api/handlers/posts_handler_test.go
+++ 025-go-api-echo/cmd/api/handlers/posts_handler_test.go
@@ -2,6 +2,8 @@ package handlers
 
 import(
 	"testing"
+	"string"
+	"io/ioutil"
 
 	"github.com/stretchr/testify/suite"
 )
@@ -16,4 +18,14 @@ func TestEndToEndSuite(t *testing.T){
 
 func (s *EndToEndSuite) TestPostHandler(){
 	c := http.Client{}
+	r,_  := c.Get("http://localhost:1323/posts")
+	s.Equal(http.StatusOk, r.StatusCode)
+}
+
+func (s *EndToEndSuite) TestPostNoResult(){
+	c := http.Client{}
+	r,_  := c.Get("http://localhost:1323/posts/229")
+	s.Equal(http.StatusOk, r.StatusCode)
+	b,_ := ioutill.ReadAll(r.body)
+	s.JSONEq(`{"status": "ok","data":[]}`,string(b))
 }
